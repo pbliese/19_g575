0decf5d7e2096470293485bc6f65972d2e084252
pbliese <42621549+pbliese@users.noreply.github.com>
2019-02-04 17:30:57 -0600

    Update main.js
    added cities and populations for table, copied table code from canvas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,70 @@
-javascript
+//javascript
+//St. Louis 308,626 -2
+//Madison 255,214 -1
+//Estes Park 6,339 -4
+//Kirksville 17,536 -3
+//Fort Collins 165080 -5
+//initialize function called when the script loads
+function initialize(){
+    cities();
+};
+
+//function to create a table with cities and their populations
+function cities(){
+    //define two arrays for cities and population
+    var cities = [
+        'Madison',
+        'St. Louis',
+        'Kirksville',
+        'Estes Park',
+		'Fort Collins'
+    ];
+    var population = [
+        255214,
+        308626,
+        17536,
+        6339,
+		165080
+    ];
+
+    //create the table element
+    var table = document.createElement("table");
+
+    //create a header row
+    var headerRow = document.createElement("tr");
+
+    //add the "City" column
+    var cityHeader = document.createElement("th");
+    cityHeader.innerHTML = "City";
+    headerRow.appendChild(cityHeader);
+
+    //add the "Population" column
+    var popHeader = document.createElement("th");
+    popHeader.innerHTML = "Population";
+    headerRow.appendChild(popHeader);
+
+    //add the row to the table
+    table.appendChild(headerRow);
+
+    //loop to add a new row for each city
+    for (var i = 0; i < cities.length; i++){
+        var tr = document.createElement("tr");
+
+        var city = document.createElement("td");
+        city.innerHTML = cities[i];
+        tr.appendChild(city);
+
+        var pop = document.createElement("td");
+        pop.innerHTML = population[i];
+        tr.appendChild(pop);
+
+        table.appendChild(tr);
+    };
+
+    //add the table to the div in index.html
+    var mydiv = document.getElementById("mydiv");
+    mydiv.appendChild(table);
+};
+
+//call the initialize function when the window has loaded
+window.onload = initialize();
